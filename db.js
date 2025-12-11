b74c3bb2be5e2158ed4682f6b4e4ccc1f6411962
GitJezequielB <jebueso@protonmail.com>
2025-12-11 00:42:43 -0600

    db

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 
 // Use environment variables for database configuration
 const dbConfig = {
-    host: process.env.DB_HOST || '34.174.64.218',
-    user: process.env.DB_USER || 'uefdglongqgvo',
-    password: process.env.DB_PASSWORD || 'Axalon2025#..',
-    database: process.env.DB_DATABASE || 'dbqygl8bcsfkjh'
+    host: process.env.DB_HOST || 'asdasd',
+    user: process.env.DB_USER || 'asdasdasd',
+    password: process.env.DB_PASSWORD || 'asdasdasd',
+    database: process.env.DB_DATABASE || 'asdasdasdas'
 };
 
 const pool = mysql.createPool(dbConfig);
